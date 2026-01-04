76a1c6644cdb90614e17a748845e39b575158a95
Chiranjeevi <chiranjeevir@example.com>
2026-01-05 04:20:41 +0530

    Update styles and components

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -8,7 +8,7 @@ export default function About() {
   return (
     <section className="about-wrapper">
       <div className="container">
-        <div className="about-bg">`   `
+        <div className="about-bg">
           <div className="about-content">
             <h2>Amar Correa Associates, Juris</h2>
 
